dfe81f34d6f51e9f504cf9fd2c0612579f8ac284
tmeagher <tmeagher1500@gmail.com>
2019-11-24 21:32:14 -0500

    Committing - Modified Login and added Login2

--- src/main/java/com/saucedemo/smoke/Login.java
+++ src/main/java/com/saucedemo/smoke/Login.java
@@ -2,9 +2,12 @@ package com.saucedemo.smoke;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
 
 public class Login {
 	
@@ -20,8 +23,26 @@ public class Login {
 		
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		//driver.manage().window().maximize();
+		driver.manage().window().maximize();
+		
 		driver.get(url);
+		String expectedTitle = "Swag Labs";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(expectedTitle, actualTitle);
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("password")).sendKeys("secret-sauce");
+		String expectedTitle2 = "Swag Labs";
+		String actualTitle2 = driver.getTitle();
+		Assert.assertEquals(expectedTitle2, actualTitle2);
+		
+		
+		driver.quit();
+		
+		
+		
+		
+		
 	}
 
 }
